0bcab9f87410e5d60aff0df611c022f7c0bfea3e
Naveencj11 <91059126+Naveencj11@users.noreply.github.com>
2026-06-19 23:37:50 +0530

    Added SmallestNumber.java

---
+++ Arrays/SmallestNumber.java
@@ -0,0 +1,15 @@
+public class SmallestNumber {
+    public static void main(String[] args) {
+        int[] arr = {10, 45, 23, 89, 12};
+
+        int smallest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest Number: " + smallest);
+    }
+}
