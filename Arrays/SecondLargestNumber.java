72fa56018f1993c56ad04835c7fc8ca623aa6cc7
Naveencj11 <91059126+Naveencj11@users.noreply.github.com>
2026-06-19 23:32:25 +0530

    Added SecondLargestNumber.java

---
+++ Arrays/SecondLargestNumber.java
@@ -0,0 +1,20 @@
+public class SecondLargestNumber {
+    public static void main(String[] args) {
+        int[] arr = {10, 45, 23, 89, 12};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Largest Number: " + largest);
+        System.out.println("Second Largest Number: " + secondLargest);
+    }
+}
