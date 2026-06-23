234345dd0f0253ac3a24ffaa12b530e15b9f1971
Naveencj11 <91059126+Naveencj11@users.noreply.github.com>
2026-06-23 13:13:48 +0530

    Added Remove Spaces program

---
+++ Strings/RemoveSpaces.java
@@ -0,0 +1,9 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Java Programming Language";
+
+        String result = str.replace(" ", "");
+
+        System.out.println("After removing spaces: " + result);
+    }
+}
